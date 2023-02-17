0e7e7c505e169371005a8b2fd6917469dfd7d32c
mbhagwan <mohit.bhagwani@oracle.com>
2023-02-17 20:46:25 +0530

    first commit

---
+++ src/FactoryDesignPattern/CellularPlan.java
@@ -0,0 +1,9 @@
+package FactoryDesignPattern;
+
+abstract class CellularPlan {
+    protected double rate;
+    abstract void getRate();
+    public void processBill(int minutes){
+        System.out.println(minutes*rate);
+    }
+}
---
+++ src/FactoryDesignPattern/PQRNetwork.java
@@ -0,0 +1,4 @@
+package FactoryDesignPattern;
+
+public class PQRNetwork {
+}
